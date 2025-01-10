d85570a34159508bfb868c267f120703814ec063
Ndomi <ndomi>
2025-01-10 11:19:09 +0200

    Editing Variables

---
+++ backend.tf
@@ -0,0 +1,25 @@
+resource "aws_dynamodb_table" "dynamodb-lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    Name = "terraform-state-lock"
+  }
+}
+
+terraform {
+    backend "s3" {
+        bucket         = "terraform-state-lock-fastapi"
+        key            = "terraform.tfstate"
+        region         = "eu-west-1"
+        dynamodb_table = "terraform-state-lock"
+    }
+}
+
+provider "aws" {
+    region = var.region
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable region {
+    type = string
+    default = "eu-west-1"
+}
+
+variable vpc_cidr {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable project {
+    type = string
+    default = "EKS FastApi"
+}
